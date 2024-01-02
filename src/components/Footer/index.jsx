8169691546c08254ec2074bc384f52a4eed05901
erol-mahmutoglu <erol.mahmutoglu@ilgilen.com.tr>
2024-01-02 23:57:33 +0300

    Fix styling issues

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,12 +2,12 @@ const Footer = () => {
   return (
     <footer className='w-full h-16 flex flex-wrap items-center justify-center gap-1 bg-slate-100 dark:bg-slate-700 text-slate-900 dark:text-slate-100 px-4'>
       <div className='flex items-center justify-center gap-1'>
-        <span className='text-slate-600 dark:text-slate-400 font-semibold text-md'>
+        <span className='text-slate-600 dark:text-slate-100 font-semibold text-md'>
           &copy; {new Date().getFullYear()}
         </span>
         <span className='flex items-center justify-center gap-1'>
           <a
-            className='text-blue-500 dark:text-blue-400 font-semibold text-md'
+            className='text-blue-600  dark:text-blue-100 font-semibold text-md'
             href='https://github.com/erol1098/turkey-weather-map'
             target='_blank'
           >
@@ -16,11 +16,11 @@ const Footer = () => {
         </span>
       </div>
       <div className='flex items-center justify-center gap-1'>
-        <span className='text-slate-600 dark:text-slate-400 font-semibold text-md'>
+        <span className='text-slate-600 dark:text-slate-100 font-semibold text-md'>
           by
         </span>
         <a
-          className='text-slate-600 dark:text-slate-400 font-bold text-md'
+          className='text-slate-600 dark:text-slate-100 font-bold text-md'
           href='https://www.linkedin.com/in/erol-mahmutoglu/'
           target='_blank'
         >
